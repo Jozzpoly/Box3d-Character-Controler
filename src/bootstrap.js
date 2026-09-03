e0477b7ec1715ba2aa1101126104f9f40c223530
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-03 21:56:27 +0200

    Expose E14 contextual authority lab route

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,6 +4,8 @@ if (requestedMode === 'balance' || requestedMode === 'e3') {
   import('./e3-balance-browser.js');
 } else if (requestedMode === 'reaction' || requestedMode === 'e14') {
   import('./e14-reaction-browser.js');
+} else if (requestedMode === 'e14lab' || requestedMode === 'contextual') {
+  import('./e14-lab-browser.js');
 } else {
   import('./main.js');
 }
