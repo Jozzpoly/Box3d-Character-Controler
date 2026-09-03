9c81c7caa9fc276d21a1a06ccd2c3f2a4063cc22
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-03 20:16:55 +0200

    Route isolated E14 reaction surface

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,8 @@ const requestedMode = new URLSearchParams(window.location.search).get('mode');
 
 if (requestedMode === 'balance' || requestedMode === 'e3') {
   import('./e3-balance-browser.js');
+} else if (requestedMode === 'reaction' || requestedMode === 'e14') {
+  import('./e14-reaction-browser.js');
 } else {
   import('./main.js');
 }
